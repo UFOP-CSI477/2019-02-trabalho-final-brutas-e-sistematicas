9e98a2e6f27546aba78d1ea21d21707022624826
Carlos Abreu <carlosabreu@192.168.0.124>
2019-11-25 16:51:21 -0300

    change dir skillhunt

--- jobs4u/resources/views/welcome.blade.php
+++ jobs4u/resources/views/welcome.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app', ['class' => 'bg-default'])
 
 @section('content')
-    <div class="header bg-gradient-primary py-7 py-lg-8">
-        <div class="container">
+    <div class="header bg-gradient-primary py-3 py-lg-5">
+        <div class="container-fluid">
             <div class="header-body text-center mt-7 mb-7">
-                <div class="row justify-content-center">
-                    <div class="col-lg-5 col-md-6">
-                        {{-- #TODO Criar View Welcolme => Busca de prestadores de serviço por proximidade e tipo de serviço desejado --}}
-                        <h1 class="text-white">{{ __('Jobs4U') }}</h1>
+                <div class="row justify-content-center m-0 p-0">
+                    <div class="col-12 m-0 p-0">
+                        <div class="card bg-secondary shadow">
+                            <div class="card-header bg-white border-0">
+                                <div class="row align-items-center m-0 p-0">
+                                    <h3 class="col-12 mb-0">{{ __('Editar Perfil') }}</h3>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        
         <div class="separator separator-bottom separator-skew zindex-100">
             <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
